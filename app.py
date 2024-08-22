ad8022a00e69982248b6016b93874c1593f9b3c1
Shahryar <shahryar@DESKTOP-EQ55Q8H>
2024-08-22 11:37:25 +0500

    Added a register page

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+from crypt import methods
 import os
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import *
 
 # Configure application
 app = Flask(__name__)
@@ -24,3 +27,22 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "Post":
+        pass
+    else:
+        return render_template("login.html")
+    
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("register.html")
+    
+@app.route("/", methods=["GET", "POST"])
+@login_required
+def home():
+    render_template("home.html")
